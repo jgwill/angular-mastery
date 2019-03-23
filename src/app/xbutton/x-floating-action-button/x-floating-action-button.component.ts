6e74999dfe56a60826027a1955a4222a6936978b
Guillaume Isabelle <jgi@jgwill.com>
2019-03-23 18:34:28 -0400

    Floating Action Button

---
+++ src/app/xbutton/x-floating-action-button/x-floating-action-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-x-floating-action-button',
+  templateUrl: './x-floating-action-button.component.html',
+  styleUrls: ['./x-floating-action-button.component.scss']
+})
+export class XFloatingActionButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
