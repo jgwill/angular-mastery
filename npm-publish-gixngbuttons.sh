5af04a298fe7caadd8a1b48ed45a2ab0e924fb64
Guillaume Isabelle <jgi@jgwill.com>
2019-03-29 07:04:50 -0400

    gixngbuttons fixed naming + new packaging

---
+++ npm-publish-gixngbuttons.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Entering GIXNGButtons Distribution folder"
+cd ./dist/gixngbuttons/
+echo "Publishing GIXNGButtons to NPM"
+npm publish
+
+echo "Publishing is in progress"
+
+sleep 15
